b881b70341125d0722a78628d9560fc538941b10
nidran <nidran@github.com>
2017-09-01 07:48:12 +0530

    Part 3 Added

---
+++ locallibrary/catalog/admin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Author, Genre, Book, BookInstance
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
